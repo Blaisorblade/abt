c75e269aa3bf76b50a6479a9cfe383f34abbfe0f
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2015-08-10 17:38:21 +0200

    Cleanup object nesting
    Lambda is now a separate module.

--- abt.scala
+++ abt.scala
@@ -6,7 +6,7 @@ import scalaz.Functor
 import scalaz.Foldable
 import scalaz.Monoid
 
-trait ABTs {
+object ABTs {
   //Not part of Scalaz, unless we switch to Scalaz's Set.
   implicit def setMonoid[T] =
     new Monoid[Set[T]] {
@@ -15,103 +15,108 @@ trait ABTs {
     }
   type Name = String
   type Vars = Set[Name]
-  /* XXX: Don't use I for interfaces. What's the right convention? */
-  trait IAbt[Signature[T]] {
-    outer =>
+}
 
-    //Concrete type used to build terms.
-    sealed trait BindingTerm[T]
-    case class Var[A](n: Name) extends BindingTerm[A]
-    case class Abs[T](n: Name, t: T) extends BindingTerm[T]
-    case class Tm[T](t: Signature[T]) extends BindingTerm[T]
-    def map[A, B](bt: BindingTerm[A])(f: A => B): BindingTerm[B]
+import ABTs._
 
-    object BindingTerm {
-      implicit val isFunctor =
-        new Functor[BindingTerm] {
-          def map[A, B](bt: BindingTerm[A])(f: A => B): BindingTerm[B] = outer.map(bt)(f)
-        }
-    }
+/* XXX: Don't use I for interfaces. What's the right convention? */
+trait IAbt[Signature[T]] {
+  outer =>
 
-    //Abstract type of terms
-    type Term
+  //Concrete type used to build terms.
+  sealed trait BindingTerm[T]
+  case class Var[A](n: Name) extends BindingTerm[A]
+  case class Abs[T](n: Name, t: T) extends BindingTerm[T]
+  case class Tm[T](t: Signature[T]) extends BindingTerm[T]
+  def map[A, B](bt: BindingTerm[A])(f: A => B): BindingTerm[B]
 
-    //Term is isomorphic to BindingTerm[Term]
-    def into(t: BindingTerm[Term]): Term
-    def out(t: Term): BindingTerm[Term]
-
-    def freeVars(t: Term): Vars
-
-    def makeVar(n: Name): Term
-    def makeAbs(n: Name, body: Term): Term
-    def makeTm(t: Signature[Term]): Term
-    def subst(outer: Term, v: Name, inner: Term): Term
-  }
-
-  class Abt[Signature[_]: Functor: Foldable] extends IAbt[Signature] {
-    def map[A, B](bt: BindingTerm[A])(f: A => B): BindingTerm[B] = bt match {
-      case Var(n) => Var(n)
-      case Abs(n, body) => Abs(n, f(body))
-      case Tm(t) => Tm(Functor[Signature].map(t)(f))
-    }
-
-    case class Term(vars: Vars, t: BindingTerm[Term])
-
-    def into(t: BindingTerm[Term]): Term =
-      t match {
-        case Var(n) => makeVar(n)
-        case Abs(n, body) => makeAbs(n, body)
-        case Tm(t) => makeTm(t)
-      }
-
-    def out(t: Term): BindingTerm[Term] = t.t
-
-    def makeVar(n: Name): Term = Term(Set(n), Var(n))
-    def makeAbs(n: Name, body: Term): Term =
-      Term(freeVars(body) - n, Abs(n, body))
-
-    def makeTm(t: Signature[Term]): Term =
-      Term(Foldable[Signature].fold(Functor[Signature].map(t)(freeVars)), Tm(t))
-
-    def freeVars(t: Term): Vars = t.vars
-
-    var index = 0
-    def fresh(): Name = {
-      index += 1
-      "x" + index
-    }
-    def fresh(baseName: Name, vars: Vars): Name =
-      if (vars contains baseName)
-        fresh(baseName + "'", vars)
-      else
-        baseName
-
-    def subst(outer: Term, v: Name, inner: Term): Term =
-      subst(outer, v, inner, true)
-
-    //This is a basic substitution with quadratic complexity.
-    def subst(outer: Term, v: Name, inner: Term, preRename: Boolean): Term =
-      out(outer) match {
-        case Var(name) if v == name => inner
-        case Tm(body) =>
-          makeTm(Functor[Signature].map[Term, Term](body)(x => subst(x, v, inner, preRename)))
-        case Abs(name, body) if v != name =>
-          val (name1, body1) =
-            if (preRename) {
-              val newName = fresh(name, freeVars(body) ++ freeVars(outer))
-              val newBody = subst(body, name, makeVar(newName), false)
-              (newName, newBody)
-            } else {
-              //We're replacing
-              (name, body)
-            }
-          val body2 = subst(body1, v, inner, preRename)
-          makeAbs(name1, body2)
-        case _ => //For when guards fail
-          outer
+  object BindingTerm {
+    implicit val isFunctor =
+      new Functor[BindingTerm] {
+        def map[A, B](bt: BindingTerm[A])(f: A => B): BindingTerm[B] = outer.map(bt)(f)
       }
   }
 
+  //Abstract type of terms
+  type Term
+
+  //Term is isomorphic to BindingTerm[Term]
+  def into(t: BindingTerm[Term]): Term
+  def out(t: Term): BindingTerm[Term]
+
+  def freeVars(t: Term): Vars
+
+  def makeVar(n: Name): Term
+  def makeAbs(n: Name, body: Term): Term
+  def makeTm(t: Signature[Term]): Term
+  def subst(outer: Term, v: Name, inner: Term): Term
+}
+
+class Abt[Signature[_]: Functor: Foldable] extends IAbt[Signature] {
+  def map[A, B](bt: BindingTerm[A])(f: A => B): BindingTerm[B] = bt match {
+    case Var(n) => Var(n)
+    case Abs(n, body) => Abs(n, f(body))
+    case Tm(t) => Tm(Functor[Signature].map(t)(f))
+  }
+
+  case class Term(vars: Vars, t: BindingTerm[Term])
+
+  def into(t: BindingTerm[Term]): Term =
+    t match {
+      case Var(n) => makeVar(n)
+      case Abs(n, body) => makeAbs(n, body)
+      case Tm(t) => makeTm(t)
+    }
+
+  def out(t: Term): BindingTerm[Term] = t.t
+
+  def makeVar(n: Name): Term = Term(Set(n), Var(n))
+  def makeAbs(n: Name, body: Term): Term =
+    Term(freeVars(body) - n, Abs(n, body))
+
+  def makeTm(t: Signature[Term]): Term =
+    Term(Foldable[Signature].fold(Functor[Signature].map(t)(freeVars)), Tm(t))
+
+  def freeVars(t: Term): Vars = t.vars
+
+  var index = 0
+  def fresh(): Name = {
+    index += 1
+    "x" + index
+  }
+  def fresh(baseName: Name, vars: Vars): Name =
+    if (vars contains baseName)
+      fresh(baseName + "'", vars)
+    else
+      baseName
+
+  def subst(outer: Term, v: Name, inner: Term): Term =
+    subst(outer, v, inner, true)
+
+  //This is a basic substitution with quadratic complexity.
+  def subst(outer: Term, v: Name, inner: Term, preRename: Boolean): Term =
+    out(outer) match {
+      case Var(name) if v == name => inner
+      case Tm(body) =>
+        makeTm(Functor[Signature].map[Term, Term](body)(x => subst(x, v, inner, preRename)))
+      case Abs(name, body) if v != name =>
+        val (name1, body1) =
+          if (preRename) {
+            val newName = fresh(name, freeVars(body) ++ freeVars(outer))
+            val newBody = subst(body, name, makeVar(newName), false)
+            (newName, newBody)
+          } else {
+            //We're replacing
+            (name, body)
+          }
+        val body2 = subst(body1, v, inner, preRename)
+        makeAbs(name1, body2)
+      case _ => //For when guards fail
+        outer
+    }
+}
+
+object Lambda {
   trait SimpleType
   case object Base extends SimpleType
   case class Arrow(t1: SimpleType, t2: SimpleType) extends SimpleType
