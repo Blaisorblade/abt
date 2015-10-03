1c0f7c9e04f8125d55701a1f71354d3750664f65
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2015-10-04 00:52:34 +0200

    Reorganize tests

--- test-src/abt/AbtTests.scala
+++ test-src/abt/AbtTests.scala
@@ -8,20 +8,25 @@ import org.scalatest.FunSuite
  * @author pgiarrusso
  */
 class AbtTests extends FunSuite {
-  test("alpha-equiv works") {
-    assert(
-      Lam("x", Var("x")) alphaEquiv Lam("y", Var("y")))
+  test("alpha-equiv distinguishes different terms") {
     assert(
       !(Lam("x", App(Var("x"), Var("x"))) alphaEquiv
         Lam("y", Var("y"))))
+  }
+
+  test("alpha-equiv works on closed terms") {
+    assert(
+      Lam("x", Var("x")) alphaEquiv Lam("y", Var("y")))
     assert(
       Lam("x", App(Var("x"), Var("x"))) alphaEquiv
         Lam("y", App(Var("y"), Var("y"))))
+  }
+
+  test("alpha-equiv works on open terms") {
     assert(!(Lam("x", Var("x")) alphaEquiv Lam("y", Var("x"))))
     assert(!(Lam("x", Var("x")) alphaEquiv Lam("x", Var("y"))))
     assert(!(Lam("y", Var("x")) alphaEquiv Lam("x", Var("y"))))
   }
-
 
   test("substitution testcases") {
     assert(Lam("x", Var("y")) subst ("y", Var("x")) alphaEquiv Lam("y", Var("x")))
